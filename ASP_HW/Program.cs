f206991a07174719c120ad2c4cef7192c2eb9297
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2025-02-27 20:29:45 +0200

    Update Program.cs

--- ASP_HW/Program.cs
+++ ASP_HW/Program.cs
@@ -12,6 +12,6 @@ app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
